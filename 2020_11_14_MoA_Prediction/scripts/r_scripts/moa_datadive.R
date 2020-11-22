8221d5b7491adb58659cfc3cd442bce8f4800803
ShaunPorwal <shaun.porwal@gmail.com>
2020-11-21 19:27:50 -0500

    Add file

---
+++ 2020_11_14_MoA_Prediction/scripts/r_scripts/moa_datadive.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+path_to_data_folder <- '~/kaggle_data/lish-moa/'
+
+
+sample_submission <- read.csv('~/kaggle_data/lish-moa/sample_submission.csv')
+test_features <- read.csv('~/kaggle_data/lish-moa/test_features.csv')
+train_drug <- read.csv('~/kaggle_data/lish-moa/train_drug.csv')
+train_features <- read.csv('~/kaggle_data/lish-moa/train_features.csv')
+train_targets_nonscored <- read.csv('~/kaggle_data/lish-moa/train_targets_nonscored.csv')
+train_targets_scored <- read.csv('~/kaggle_data/lish-moa/train_targets_scored.csv')
+
+
+
+
